2dc04afd013983ad5f351d858af924aca9c0cee7
momanyshev <maksimmanyshev@yandex.ru>
2023-05-01 14:00:53 +0300

    Project HabrTest added

---
+++ src/test/java/com/example/habrtest/MainPageTest.java
@@ -0,0 +1,64 @@
+package com.example.habrtest;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+import java.util.List;
+
+public class MainPageTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.habr.com/");
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void companySearchTest() {
+
+        WebElement companiesLink = driver.findElement(By.xpath("//a[@href='/ru/companies/']"));
+        companiesLink.click();
+
+        WebElement searchQuery = driver.findElement(By.cssSelector("input[placeholder='Поиск']"));
+        searchQuery.sendKeys("Сбер");
+
+        WebElement companyProfile = driver.findElement(By.xpath("//em[(text()='Сбер')]"));
+        companyProfile.click();
+
+        List<WebElement> companyWebsite = (driver.findElements(By.xpath("//a[contains(text(),'www.sber.ru')]")));
+        assertFalse(companyWebsite.isEmpty(), "Ссылка на сайт компании отсутствует");
+    }
+
+    @Test
+    public void docsRulesTest() {
+
+        WebElement docsLink = driver.findElement(By.cssSelector("a[href='/ru/docs/docs/transparency/']"));
+        docsLink.click();
+
+        WebElement rolesRules = driver.findElement(By.xpath("//a[contains(text(),'Правила оказания рекламных услуг')]"));
+        rolesRules.click();
+
+        List<WebElement> companyWebsite = (driver.findElements(By.xpath("//h2[text()='Правила оказания рекламных услуг']")));
+        assertFalse(companyWebsite.isEmpty(), "Ссылка на Правила оказания рекламных услуг недействительна");
+    }
+}
